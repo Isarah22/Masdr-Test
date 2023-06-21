21410b0de25edcf9c9b5f707e389a4a6c02ee628
Sarah Alenizi <88673299+Isarah22@users.noreply.github.com>
2023-06-21 20:02:41 +0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,16 @@
+
+links.forEach(link => {
+    link.addEventListener("click", e => {
+       e.preventDefault(); //preventing form submit
+       forms.classList.toggle("show-signup");
+    })
+    {
+   
+    }
+})
+
+
+   function click(){
+        console.log("Congratulations! you are now signed up");
+   
+   }
